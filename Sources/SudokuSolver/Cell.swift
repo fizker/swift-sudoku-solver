4485d1604444e32da7e3d10416d7241b95b4ac4a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-08-26 13:38:37 +0200

    Added convenience struct for cell coordinate

--- Sources/SudokuSolver/Cell.swift
+++ Sources/SudokuSolver/Cell.swift
@@ -35,11 +35,39 @@ public struct Row: Container {
 	public let position: Int
 }
 
+public struct Coordinate: Equatable {
+	var row: Int
+	var column: Int
+	var group: Int
+
+	init(row: Int, column: Int, group: Int) {
+		self.row = row
+		self.column = column
+		self.group = group
+	}
+
+	init(row: Int, column: Int) {
+		self.row = row
+		self.column = column
+
+		// This is now 0 for 1-3, 3 for 4-6, 6 for 7-9
+		let rg = (row-1) / 3 * 3
+
+		// This is now 0 for 1-3, 1 for 4-6, 2 for 7-9
+		let cg = (column-1) / 3
+
+		self.group = cg + 1 + rg
+	}
+}
+
 public struct Cell: Equatable, CustomStringConvertible, CustomDebugStringConvertible {
 	public var value: Int?
 	public let row: Int
 	public let column: Int
 	public let group: Int
+	public var coordinate: Coordinate {
+		.init(row: row, column: column, group: group)
+	}
 
 	init(value: Int? = nil, row: Int, column: Int, group: Int) {
 		self.value = value
