da020d16e0ffdc0b5ec7e8fd4bdf0dfc13d0e274
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-08-27 21:54:07 +0200

    Naked singles are now using the pencil marks

--- Sources/SudokuSolver/Solvers/nakedSingle.swift
+++ Sources/SudokuSolver/Solvers/nakedSingle.swift
@@ -1,18 +1,9 @@
 /// A naked single is when a cell can only contain one digit because other cells in either column, row or
 /// group have eliminated the other digits.
 func nakedSingle(puzzle: Puzzle) -> Puzzle {
-	var puzzle = puzzle
-
-	for var cell in puzzle.cells {
-		guard !cell.hasValue
-		else { continue }
-
-		let candidates = puzzle.candidates(for: cell)
-
-		if candidates.count == 1 {
-			cell.value = candidates[0]
-			return puzzle.updating(cell)
-		}
+	for var cell in puzzle.cells where cell.pencilMarks.count == 1 {
+		cell.value = cell.pencilMarks.first!
+		return puzzle.updating(cell)
 	}
 
 	return puzzle
