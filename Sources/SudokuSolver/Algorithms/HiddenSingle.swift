a5c2d5dbed60135f2224ee82fd03ab7fcf94bfe3
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-08-28 15:02:10 +0200

    Refactored file structure to have Algorithms

--- Sources/SudokuSolver/Algorithms/HiddenSingle.swift
+++ Sources/SudokuSolver/Algorithms/HiddenSingle.swift
@@ -7,16 +7,9 @@ struct HiddenSingle: Algorithm {
 			let cellCandidates = puzzle.candidates(for: cell)
 
 			for container in puzzle.containers(for: cell) {
-				var containerCandidates = [1,2,3,4,5,6,7,8,9].map { (value: $0, potentialCount: 0) }
+				let containerCandidates = puzzle.candidateCount(for: container)
 
-				for cell in container.cells {
-					for value in cell.pencilMarks {
-						let index = value - 1
-						containerCandidates[index].potentialCount += 1
-					}
-				}
-
-				for containerCandidate in containerCandidates where containerCandidate.potentialCount == 1 {
+				for containerCandidate in containerCandidates where containerCandidate.count == 1 {
 					if cellCandidates.contains(containerCandidate.value) {
 						cell.value = containerCandidate.value
 						return puzzle.updating(cell)
