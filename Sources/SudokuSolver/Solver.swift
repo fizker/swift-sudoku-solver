2770f0b18109e764e9f0d1db542d30d648dfaa2a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-09-13 01:47:48 +0200

    Removed the Solver class and rolled the `.solve()` method into `[Algorithm]` as an extension method

--- Sources/SudokuSolver/Solver.swift
+++ Sources/SudokuSolver/Solver.swift
@@ -1,17 +1,28 @@
-protocol Algorithm {
+public protocol Algorithm {
+	/// The name of the algorithm.
 	static var name: String { get }
 
 	init()
 
+	/// Executes the algorithm against the given puzzle.
+	///
+	/// The puzzle is returned unchanged if the algorithm failed to find the required state.
+	///
+	/// - parameter puzzle: The puzzle to examine.
+	/// - returns: The puzzle after applying the algorithm. The puzzle might not have changed.
 	func callAsFunction(_ puzzle: Puzzle) -> Puzzle
 }
 
-extension Algorithm {
+public extension Algorithm {
+	/// The name of the algorithm.
 	var name: String { Self.name }
 }
 
-public class Solver {
-	private let algorithms: [Algorithm] = [
+public extension Array where Element == any Algorithm {
+	/// All supported algorithms.
+	///
+	/// They are ordered somewhat for performance, so that cheaper or more common algorithms are attempted first.
+	static var all: [any Algorithm] { [
 		NakedSingle(),
 		HiddenSingle(),
 		PointingPair(),
@@ -21,19 +32,22 @@ public class Solver {
 		XWing(),
 		YWing(),
 		NakedTriple(),
-	]
+	] }
 
-	private var puzzle: Puzzle
-
-	public init(puzzle: Puzzle) {
-		self.puzzle = puzzle
-	}
-
-	public func solve() -> Puzzle {
+	/// Attempts to solve the given puzzle using the algorithms that this array contains.
+	///
+	/// The algorithms are attempted one at a time, in order. If the algorithm changes the puzzle in any way, the loop is restarted and all algorithms are retried in order against the new puzzle state.
+	///
+	/// The loop ends if the puzzle is considered solved or if no algorithms were capable of updating the puzzle.
+	///
+	/// - parameter puzzle: The puzzle to solve.
+	/// - returns: The most solved state of the puzzle, given the available algorithms.
+	func solve(_ puzzle: Puzzle) -> Puzzle {
+		var puzzle = puzzle
 		puzzle.pencilMarkKnownValues()
 
 		runloop: while !puzzle.isSolved {
-			solverloop: for algorithm in algorithms {
+			solverloop: for algorithm in self {
 				let s = algorithm(puzzle)
 				if s != puzzle {
 					puzzle = s
@@ -48,6 +62,5 @@ public class Solver {
 }
 
 public func solve(_ p: Puzzle) -> Puzzle {
-	let solver = Solver(puzzle: p)
-	return solver.solve()
+	return [Algorithm].all.solve(p)
 }
