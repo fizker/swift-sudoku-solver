13be9cc6526311c9588af3d2f06a889df3d70698
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-09-16 10:49:02 +0200

    Refactored HiddenSingle algorithm to reduce complexity by a factor of more than 10

--- Sources/SudokuSolver/Algorithms/HiddenSingle.swift
+++ Sources/SudokuSolver/Algorithms/HiddenSingle.swift
@@ -1,21 +1,25 @@
 /// A hidden single is where only one cell in a column, row or box can contain a specific digit.
+///
+/// The result of a match finds a digit.
+///
+/// - Complexity: O(27(738 + 18)) -> O(20.412) for a regular 9x9 sudoku puzzle.
 struct HiddenSingle: Algorithm {
 	static let name = "Hidden single"
 
 	func callAsFunction(_ puzzle: Puzzle) -> Puzzle {
-		for var cell in puzzle.cells where !cell.hasValue {
-			let cellCandidates = cell.pencilMarks
+		// O(27)
+		for container in puzzle.containers {
+			// O(c+cn)
+			let containerCandidates = container.candidateCount
 
-			for container in puzzle.containers(for: cell) {
-				let containerCandidates = container.candidateCount
+			// O(9)
+			guard let candidate = containerCandidates.first(where: { $0.count == 1 })
+			else { continue }
 
-				for containerCandidate in containerCandidates where containerCandidate.count == 1 {
-					if cellCandidates.contains(containerCandidate.value) {
-						cell.value = containerCandidate.value
-						return puzzle.updating(cell)
-					}
-				}
-			}
+			// O(9)
+			var cell = container.cells.first { $0.pencilMarks.contains(candidate.value) }!
+			cell.value = candidate.value
+			return puzzle.updating(cell)
 		}
 
 		return puzzle
