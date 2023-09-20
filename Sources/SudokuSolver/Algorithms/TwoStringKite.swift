79ffa46918261fef667a29514dbf9555f73d5f60
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-09-20 22:46:22 +0200

    Minor refactor of TwoStringKite for a performance boost

--- Sources/SudokuSolver/Algorithms/TwoStringKite.swift
+++ Sources/SudokuSolver/Algorithms/TwoStringKite.swift
@@ -5,13 +5,18 @@
 ///
 /// Any cells that then see both of their counterparts cannot have the value, since that would force both A1 and B1 to have the same value.
 ///
-/// - Complexity: O(180 + 9(738 + 9 + 9(9 + 3.708 + 18(18(162 + 14 + 14))))) -> O(5.294.340) for a reguler 9x9 sudoku puzzle.
+/// - Complexity: O(522 + 9(738 + 9 + 9(9 + 3.024 + 18(18(162 + 14 + 14))))) -> O(5.239.278	) for a reguler 9x9 sudoku puzzle.
 struct TwoStringKite: Algorithm {
 	static let name = "2-String Kite"
 
 	func callAsFunction(_ puzzle: Puzzle) -> Puzzle {
-		// 180 + O(9)
-		for box in puzzle.boxes {
+		// O(171 + 171 + 180)
+		let columns = puzzle.columns
+		let rows = puzzle.rows
+		let boxes = puzzle.boxes
+
+		// O(9n)
+		for box in boxes {
 			// O(738 + 9), returns 9
 			let twoCandidates = box.candidateCount.filter { $0.count == 2 }.map(\.value)
 
@@ -20,9 +25,9 @@ struct TwoStringKite: Algorithm {
 				// O(9)
 				let cells = box.cells.filter { $0.pencilMarks.contains(digit) }
 
-				// O(2 * 1.854)
-				let firstPair = cellsBeingSecondCandidate(in: puzzle, firstCandidate: cells[0], digit: digit)
-				let secondPair = cellsBeingSecondCandidate(in: puzzle, firstCandidate: cells[1], digit: digit)
+				// O(2 * 1.512)
+				let firstPair = cellsBeingSecondCandidate(firstCandidate: cells[0], digit: digit, rows: rows, columns: columns)
+				let secondPair = cellsBeingSecondCandidate(firstCandidate: cells[1], digit: digit, rows: rows, columns: columns)
 
 				guard firstPair.isNotEmpty && secondPair.isNotEmpty
 				else { continue }
@@ -55,11 +60,11 @@ struct TwoStringKite: Algorithm {
 		return puzzle
 	}
 
-	/// - Complexity: O(171 \* 2 + 2 \* 747 + 18) -> O(1.854)
+	/// - Complexity: O(2 \* 747 + 18) -> O(1.512)
 	/// - Returns: A maximum of 18 cells.
-	func cellsBeingSecondCandidate(in puzzle: Puzzle, firstCandidate cell: Cell, digit: Int) -> [Cell] {
-		// O(171 * 2 + 2(738 + 9))
-		let candidateGroups = [puzzle.rows[cell.row - 1] as any Container, puzzle.columns[cell.column - 1]]
+	func cellsBeingSecondCandidate(firstCandidate cell: Cell, digit: Int, rows: [Row], columns: [Column]) -> [Cell] {
+		// O(2(738 + 9))
+		let candidateGroups = [rows[cell.row - 1] as any Container, columns[cell.column - 1]]
 			.filter {
 				// O(738)
 				$0.candidateCount
